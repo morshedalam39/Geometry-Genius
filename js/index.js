fd206ba753ff575840c76ae7b614d84efc31b24b
morshedalam39 <morshedalam4239@gmail.com>
2023-02-18 02:24:21 +0600

    second card completed

--- js/index.js
+++ js/index.js
@@ -7,10 +7,21 @@ function inputFieldValuById(inputId){
     const inputFieldValue =parseFloat(inputFieldString);
     return inputFieldValue;
 }
+// use id get data element funcation
+
 // card title  get function
 function getTextTitleValueById(text){
     const titleValue =document.getElementById(text).innerText;
     return titleValue;
+
+}
+//multfacation funcation
+function multfacation(value1,value2){
+  const firstInputValue =inputFieldValuById(value1);
+  const secondInputValue =inputFieldValuById(value2);
+  const calculet = firstInputValue * secondInputValue;
+  return calculet;
+
 
 }
 // use funcation createElement table
@@ -18,11 +29,10 @@ function displayData(name ,total) {
     const container = document.getElementById("table-container");
     const tr = document.createElement("tr");
     tr.innerHTML = `  
-      <td>${serial} ${name}
-      </td>
-      <td>${total }</td>
+      <td>${serial} ${name}</td>
+      <td>${total}cm<sup>2</sup></td>
       <td> 
-      <button class="btn btn-info  text-white">Convert to m<sup>2</sup></button>
+      <button class="btn btn-info inline  text-white">Convert to m<sup>2</sup></button>
       </td>
     `;
     container.appendChild(tr);
@@ -32,10 +42,17 @@ function displayData(name ,total) {
 document.getElementById('first-card').addEventListener('click', function(){
     serial += 1;
     const title= getTextTitleValueById('first-title');
-    const firstInputValue =inputFieldValuById('firstCard-firstInput');
-    const secondInputValue =inputFieldValuById('firstCard-secondInput');
-
-    const calculet = 0.5 * firstInputValue * secondInputValue ;
-   displayData(title , calculet);
+    const result= 0.5 *  multfacation('firstCard-firstInput','firstCard-secondInput');
+    displayData(title , result);
  
+})
+
+// second card EventListener
+document.getElementById('second-card').addEventListener('click', function(){
+  serial += 1;
+  const title= getTextTitleValueById('second-title');
+  const result= multfacation('secondCard-firstInput','secondCard-secondInput');
+
+ displayData(title , result);
+
 })
