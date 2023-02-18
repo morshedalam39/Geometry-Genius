0bb2b61d337de20c012d8699f538d66837e64e2d
morshedalam39 <morshedalam4239@gmail.com>
2023-02-18 11:35:10 +0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 // use id get data  input feild funcation 
 let serial = 0;
-function inputFieldValuById(inputId){
+function inputFieldValuById(num1,num2){
+    console.log(num1)
+    console.log(num2)
+
+    if (num1 == "" || num2 == "") {
+      return "requard";
+    }
+   else if (num1 <= 0 || num2 <= 0 ) {
+      return "negative";
+    }
+   else if (typeof (num1) != "number" || isNaN(num1) != "number"){
+      return "num";
+    }
+    else {
+      return true;
+    }
+  
     
-    const inputField = document.getElementById(inputId);
-    const inputFieldString=inputField.value;
-    const inputFieldValue =parseFloat(inputFieldString);
-    return inputFieldValue;
 }
 // use id get data element funcation
+function textElementValueByID(elementId){
+  const textElementValue=document.getElementById(elementId);
+  const textElementValueString=textElementValue.innerText;
+  const textElement =parseFloat(textElementValueString);
+  return textElement;
+}
 
 // card title  get function
 function getTextTitleValueById(text){
@@ -15,14 +33,43 @@ function getTextTitleValueById(text){
     return titleValue;
 
 }
-//multfacation funcation
+//multfacation funcation for input value calculation first two card
+
 function multfacation(value1,value2){
-  const firstInputValue =inputFieldValuById(value1);
-  const secondInputValue =inputFieldValuById(value2);
+  console.log(value1)
+  const inputValue =inputFieldValuById(value1.value,value2.value);
+  console.log(inputValue)
+  if(inputValue =="requard"){
+    
+    return alert ('empty')
+  }
+  
+  else if (inputValue =="negative"){
+    return alert ('negative')
+  }
+  
+  else if (inputValue =="num"){
+    return alert ('isnumber')
+  }
+  console.log("no");
+  
+  const calculet = value1.value * value2.value;
+  
+
+  if (calculet %2==0){
+    return calculet;
+  }
+  else{
+    return calculet.toFixed(2);
+  }
+  
+}
+// multfacation funcation for text elemnent value calculation last foure card
+function multfacationTextElemnt(value1,value2){
+  const firstInputValue =textElementValueByID(value1);
+  const secondInputValue =textElementValueByID(value2);
   const calculet = firstInputValue * secondInputValue;
   return calculet;
-
-
 }
 // use funcation createElement table
 function displayData(name ,total) {
@@ -51,7 +98,18 @@ document.getElementById('first-card').addEventListener('click', function(){
 document.getElementById('second-card').addEventListener('click', function(){
   serial += 1;
   const title= getTextTitleValueById('second-title');
-  const result= multfacation('secondCard-firstInput','secondCard-secondInput');
+  let sfi =document.getElementById('secondCard-firstInput')
+  let ssi= document.getElementById('secondCard-secondInput')
+  const result= multfacation(sfi, ssi);
+
+ displayData(title , result);
+
+})
+// third card EventListener
+document.getElementById('third-card').addEventListener('click', function(){
+  serial += 1;
+  const title= getTextTitleValueById('third-title');
+  const result= multfacationTextElemnt('thirdCard-firstValue','thirdCard-secondValue');
 
  displayData(title , result);
 
